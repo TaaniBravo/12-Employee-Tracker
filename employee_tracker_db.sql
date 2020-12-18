f6da6b9e4e3ac97106ab721774876092d14b4f81
Taani Maama <taani1maama@live.com>
2020-12-17 18:39:09 -0800

    Created the department table.

--- employee_tracker_db.sql
+++ employee_tracker_db.sql
@@ -1,3 +1,13 @@
 DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    
+)
