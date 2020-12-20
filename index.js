2b7446a50236c9f16ca9cfce86a377e96c4c975f
Taani Maama <taani1maama@live.com>
2020-12-20 13:46:43 -0800

    Created the queryAll function.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     port: 3306,
     user: "root",
     password: "2b.D_?rtwbjm",
-    database: "greatBay_DB",
+    database: "employee_tracker_db",
 });
 
 connection.connect(function (err) {
@@ -17,59 +17,70 @@ connection.connect(function (err) {
 
 const init = () => {
     inquirer
-    .prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-            'View All Employees',
-            'View All Employees By Department',
-            'View All Employees By Management',
-            'Add Employee',
-            'Remove Employee',
-            'Update Employee Role',
-            'Update Employee Manager',
-            'EXIT'
-        ],
-    })
-    .then(answer => {
-        if (answer.action === 'View All Employees') {
-            console.log('\n -------------------------------------- \n');
-            queryAll();
-        }
-        else if (answer.action === 'View All Employees By Department') {
-            console.log('\n -------------------------------------- \n');
-            queryByDepartment();
-        }
-        else if (answer.action === 'View All Employees By Management') {
-            console.log('\n -------------------------------------- \n');
-            queryByManagement();
-        }
-        else if (answer.action === 'Add Employee') {
-            console.log('\n -------------------------------------- \n');
-            addEmployee();
-        }
-        else if (answer.action === 'Remove Employee') {
-            console.log('\n -------------------------------------- \n');
-            removeEmployee();
-        }
-        else if (answer.action === 'Update Employee Role') {
-            console.log('\n -------------------------------------- \n');
-            updateRole();
-        }
-        else if (answer.action === 'Update Employee Manager') {
-            console.log('\n -------------------------------------- \n');
-            updateEmployeeManager();
-        }
-        else {
-            connection.end();
-        };
-    });
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                'View All Employees',
+                'View All Employees By Department',
+                'View All Employees By Management',
+                'Add Employee',
+                'Remove Employee',
+                'Update Employee Role',
+                'Update Employee Manager',
+                'EXIT'
+            ],
+        })
+        .then(answer => {
+            if (answer.action === 'View All Employees') {
+                console.log('\n -------------------------------------- \n');
+                queryAll();
+            }
+            else if (answer.action === 'View All Employees By Department') {
+                console.log('\n -------------------------------------- \n');
+                queryByDepartment();
+            }
+            else if (answer.action === 'View All Employees By Management') {
+                console.log('\n -------------------------------------- \n');
+                queryByManagement();
+            }
+            else if (answer.action === 'Add Employee') {
+                console.log('\n -------------------------------------- \n');
+                addEmployee();
+            }
+            else if (answer.action === 'Remove Employee') {
+                console.log('\n -------------------------------------- \n');
+                removeEmployee();
+            }
+            else if (answer.action === 'Update Employee Role') {
+                console.log('\n -------------------------------------- \n');
+                updateRole();
+            }
+            else if (answer.action === 'Update Employee Manager') {
+                console.log('\n -------------------------------------- \n');
+                updateEmployeeManager();
+            }
+            else {
+                connection.end();
+            };
+        });
 };
 
 const queryAll = () => {
-    connection.query('SELECT * FROM employee', (err, res) => {
+    connection.query('SELECT employee.id, first_name, last_name, title, department_name, salary, manager_id FROM employee INNER JOIN role_info ON employee.role_id = role_info.id INNER JOIN department ON role_info.department_id = department.id;', (err, res) => {
         if (err) throw err;
-
-    })
+        console.table(res)
+        inquirer
+            .prompt({
+                name: "action",
+                type: "list",
+                message: "What would you like to do?",
+                choices: ['Continue', 'EXIT']
+            })
+            .then(answer => {
+                if (answer.action === 'Continue') init();
+                else connection.end();
+            });
+    });
 };
--- seed.sql
+++ seed.sql
@@ -1,20 +1,17 @@
 USE employee_tracker_db;
-
 INSERT INTO
   department (department_name)
 VALUES
   ('Management'),
   ('Engineering'),
   ('Internship');
-
 INSERT INTO
   role_info (title, salary, department_id)
 VALUES
   ("Project Lead", 100000, 1),
   ('Sr. Developer', 80000, 2),
-  ('Jr. Developer', 65000, 3);
-  ('Intern', 50000, 4);
-
+  ('Jr. Developer', 65000, 2),
+  ('Intern', 50000, 3);
 INSERT INTO
   employee (first_name, last_name, role_id, manager_id)
 VALUES
@@ -26,3 +23,15 @@ VALUES
   ('Dwayne', 'Carter', 3, 2),
   ('Jacques', 'Webster', 4, 1),
   ('Austin', 'Post', 4, 2);
+SELECT
+  employee.id,
+  first_name,
+  last_name,
+  title,
+  department_name,
+  salary,
+  manager_id
+FROM
+  employee
+  INNER JOIN role_info ON employee.role_id = role_info.id
+  INNER JOIN department ON role_info.department_id = department.id
