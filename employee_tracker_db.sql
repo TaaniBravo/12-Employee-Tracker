5acebd3351b44225d75cf646ca0f703c70aea860
Taani Maama <taani1maama@live.com>
2020-12-19 19:57:21 -0800

    Created my seed.sql so that I can create the functions easier.

--- employee_tracker_db.sql
+++ employee_tracker_db.sql
@@ -9,16 +9,18 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role_info (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(100) NOT NULL,
-    salary DECIMAL(100),
-    department_id INT
+    salary DECIMAL(65),
+    department_id INT,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT AUTO_INCREMENT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    manager_id INT,
+    PRIMARY KEY (id)
 );
---
+++ seed.sql
@@ -0,0 +1,28 @@
+USE employee_tracker_db;
+
+INSERT INTO
+  department (department_name)
+VALUES
+  ('Management'),
+  ('Engineering'),
+  ('Internship');
+
+INSERT INTO
+  role_info (title, salary, department_id)
+VALUES
+  ("Project Lead", 100000, 1),
+  ('Sr. Developer', 80000, 2),
+  ('Jr. Developer', 65000, 3);
+  ('Intern', 50000, 4);
+
+INSERT INTO
+  employee (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Marshall', 'Mathers', 1, null),
+  ('Aubrey', 'Graham', 1, null),
+  ('Kendrick', 'Duckworth', 2, 1),
+  ('Kanye', 'West', 2, 2),
+  ('Shawn', 'Carter', 3, 1),
+  ('Dwayne', 'Carter', 3, 2),
+  ('Jacques', 'Webster', 4, 1),
+  ('Austin', 'Post', 4, 2);
