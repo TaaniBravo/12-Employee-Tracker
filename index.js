df108957a8759a03c82b60ff1145a114b7642ee8
Taani Maama <taani1maama@live.com>
2020-12-21 15:02:38 -0800

    Cleaned up the code for the queryAll function.

--- index.js
+++ index.js
@@ -68,8 +68,22 @@ const init = () => {
 };
 
 const queryAll = () => {
+    const query = `SELECT
+    e.id,
+    e.first_name,
+    e.last_name,
+    r.title,
+    d.department_name,
+    r.salary,
+    CONCAT(m.first_name, ' ', m.last_name) AS manager
+  FROM
+    employee e
+    LEFT JOIN role_info r ON e.role_id = r.id
+    LEFT JOIN department d ON r.department_id = d.id
+    LEFT JOIN employee m ON m.id = e.manager_id`
+    
     connection.query(
-        `SELECT e.id, e.first_name, e.last_name, r.title, d.department_name, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e LEFT JOIN role_info r ON e.role_id = r.id LEFT JOIN department d ON r.department_id = d.id LEFT JOIN employee m ON m.id = e.manager_id`, (err, res) => {
+        query, (err, res) => {
         if (err) throw err;
         console.table(res)
         inquirer
