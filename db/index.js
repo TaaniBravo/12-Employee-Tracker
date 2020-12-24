6d31071f2b5ba3af1478c453ebcc8392f59a8c83
Taani Maama <taani1maama@live.com>
2020-12-24 11:59:20 -0800

    Created the get functions I am going to want to make this program modularized.

--- db/index.js
+++ db/index.js
@@ -1,1 +1,21 @@
-const connection = require('./connection')
+const connection = require('./connection');
+
+module.exports = {
+    getDepartments() {
+
+        return connection.query('SELECT * FROM department')
+
+    },
+
+    getRoles() {
+
+        return connection.query('SELECT * FROM role_info')
+
+    },
+
+    getEmployees() {
+
+        return connection.query('SELECT * FROM employee')
+
+    },
+}
