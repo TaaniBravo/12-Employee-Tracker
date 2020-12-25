1fa61a2844b3ab1161979c0dde27dca38ef8edc7
Taani Maama <taani1maama@live.com>
2020-12-25 11:10:35 -0800

    Cleaned up seed file.

--- sql/seed.sql
+++ sql/seed.sql
@@ -28,18 +28,3 @@ VALUES
   ('Jacques', 'Webster', 6, 2),
   ('Austin', 'Post', 7, 1),
   ('Scott', 'Mescudi', 5, 2);
-SELECT
-  e.id,
-  e.first_name,
-  e.last_name,
-  r.title,
-  d.department_name,
-  r.salary,
-  CONCAT(m.first_name, ' ', m.last_name) AS manager
-FROM
-  employee e
-  LEFT JOIN role r ON e.role_id = r.id
-  LEFT JOIN department d ON r.department_id = d.id
-  LEFT JOIN employee m ON m.id = e.manager_id
-WHERE
-  m.id = 1
